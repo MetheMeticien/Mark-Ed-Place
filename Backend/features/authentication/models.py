74daf3c0b74a4e7967c84adb66ac50a7fca9fbbd
mehreen hossain chowdhury <128203371+mehreen019@users.noreply.github.com>
2025-05-24 16:06:50 +0600

    fix: registration consistency errors fixed

--- Backend/features/authentication/models.py
+++ Backend/features/authentication/models.py
@@ -23,11 +23,11 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     gender = Column(String, nullable=True)
     role = Column(Enum(Role), default=Role.NORMAL, nullable=False)
-    #university_id = Column(String, ForeignKey('universities.id'), nullable=False)
+    university_id = Column(String, ForeignKey('universities.id'), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
     
     # Relationship with ModeratorRequest
     moderator_requests = relationship("ModeratorRequest", back_populates="user")
     # Relationships
     products = relationship("Product", back_populates="seller")
-    #university = relationship("University", back_populates="users")
+    university = relationship("University", back_populates="users")
